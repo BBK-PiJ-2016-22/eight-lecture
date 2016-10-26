035359d0a748c283da448342ddc58638a5dc68ab
Mr Casper Nielsen <cniels02@dcs.bbk.ac.uk>
2016-10-26 14:34:52 +0100

    first commit

---
+++ exercise1/SmartPhone.java
@@ -0,0 +1,24 @@
+public class SmartPhone extends MobilePhone {
+	
+	
+	public void browseWeb(String browse){
+		
+		
+		
+	}
+	
+	public void findPosition(){
+		
+		System.out.println("Your positin is: earth");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
